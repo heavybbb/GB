02d3fb683dcedd470742020f1639e6451a2b5655
Dmitriy K <cd.78@bk.ru>
2024-01-18 18:02:29 +0300

    немного изменил структуру каталогов

---
+++ Introduction/Lessons3/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+using System.Reflection.Metadata;
+
+void PrintSquares(int limit)
+
+{
+    int i =1;
+while(i <= limit)
+{
+    Console.Write($"{i*i} ");
+    i++;
+    
+}
+
+}
+
+int q=4;
+int w=2;
+int e=5;
+PrintSquares(q);
+PrintSquares(w);
+PrintSquares(e);
---
+++ Introduction/Lessons3/Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+
+}
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEvenElements(array);
+//PrintArray(array);
+Console.Write({ array});
+
+
+
+ Random  rnd = new Random();
+
+ int size=1000;
+ int[]arr_int=new int[size];
+ int i=0;
+ while(i<size)
+ {
+    arr_int[i]=rnd.Next(1,10);
+    i=++i;
+
+ }
+ i=0;
+ while(i<size)
+ {
+    Console.Write($"{arr_int[i]} ");
+    i=++i;
+ }
