759629ad3fb045eb2026abe39f6051ba925715de
Dmitriy K <cd.78@bk.ru>
2024-01-27 00:46:03 +0300

    Добавил новые примеры задач

--- Introduction/Lessons5.TwoArrays/DZ3/obj/Debug/net8.0/DZ3.AssemblyInfo.cs
+++ Introduction/Lessons5.TwoArrays/DZ3/obj/Debug/net8.0/DZ3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DZ3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24c946742debb584a4ba955cc0f7150ceb98e23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf073ffc6c2dcc7b470f334b42f861d188a4a61e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DZ3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DZ3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
