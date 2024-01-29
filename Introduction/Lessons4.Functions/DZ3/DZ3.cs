db3c114510b7d27b10b1e545dde978fae57ad702
Dmitriy Kiselev <heavybbb@gmail.com>
2024-01-30 02:13:12 +0300

    Lessons7. Task 1

--- Introduction/Lessons4.Functions/DZ3/DZ3.cs
+++ Introduction/Lessons4.Functions/DZ3/DZ3.cs
@@ -6,21 +6,21 @@ public class NEW
 {
     static void Main()
     {
-    int[] numbers = {10,11,12,13,14,15,16};
-//Передача массива в функцию
-    ReverseMassiv(numbers);
-    static void ReverseMassiv(int[] array)
-    {
-        int length = array.Length;
-        for (int i = 0; i < length; i++)
+        int[] numbers = { 10, 11, 12, 13, 14, 15, 16 };
+        //Передача массива в функцию
+        ReverseMassiv(numbers);
+        static void ReverseMassiv(int[] array)
         {
-           Console.WriteLine(array[length -1 - i]);
+            int length = array.Length;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(array[length - 1 - i]);
+            }
+
         }
-        
+
+
     }
 
-        
 }
 
-}
-        
--- Introduction/Lessons7.Recursion/DZ1/DZ1.cs
+++ Introduction/Lessons7.Recursion/DZ1/DZ1.cs
@@ -1,8 +1,40 @@
-﻿using System;
-class Tas3 {
-  static void Main() {
+﻿/* Задача 1: Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N. 
+Использовать рекурсию, не использовать циклы. */
 
+using System;
+class Program
+{
+    static void Main()
+    {
 
+        Console.Write("Введите первое число: ");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        AllNumbersFromOneToTen(firstNumber, secondNumber);
+    }
+    static void AllNumbersFromOneToTen(int firstNumber, int secondNumber)
+
+    {
+        if (firstNumber == secondNumber)
+        {
+            return;
+        }
+        if (firstNumber < secondNumber)
+        {
+            ++firstNumber;
+            Console.Write(firstNumber + " ");
+        }
+        if (firstNumber > secondNumber)
+        {
+            firstNumber--;
+            Console.Write(firstNumber + " ");
+        }
+        AllNumbersFromOneToTen(firstNumber, secondNumber);
     }
 
 }
+
