65d37c64106c841dbcee0c17093d334c87d543c9
KDA <KDA@mail.mail>
2024-01-30 10:13:52 +0300

    3

--- Introduction/Lessons6.ArrayAndString/DZ4/DZ4.cs
+++ Introduction/Lessons6.ArrayAndString/DZ4/DZ4.cs
@@ -4,23 +4,23 @@
 В полученной строке слова должны быть также разделены пробелами. */
 public class Task4
 {
-public static void Main(string[] args)
-{
-// Входная строка со словами, разделенными пробелами
-string input = "Hello my world";
-// Вызов метода для обращения порядка слов в строке
-string result = ReverseWords(input);
-// Вывод результата
-Console.WriteLine(result);
-}
-// Метод для обращения порядка слов в строке
-public static string ReverseWords(string str)
-{
-// Разделение строки на слова
-string[] words = str.Split(' ');
-// Обращение порядка слов
-Array.Reverse(words);
-// Соединение слов обратно в строку с пробелами
-return string.Join(" ", words);
-}
+    public static void Main(string[] args)
+    {
+        // Входная строка со словами, разделенными пробелами
+        string input = "Hello my world";
+        // Вызов метода для обращения порядка слов в строке
+        string result = ReverseWords(input);
+        // Вывод результата
+        Console.WriteLine(result);
+    }
+    // Метод для обращения порядка слов в строке
+    public static string ReverseWords(string str)
+    {
+        // Разделение строки на слова
+        string[] words = str.Split(' ');
+        // Обращение порядка слов
+        Array.Reverse(words);
+        // Соединение слов обратно в строку с пробелами
+        return string.Join(" ", words);
+    }
 }
