d9c1f438589754eb40ff08f906b1fb2233cc8b28
KDA <KDA@mail.mail>
2024-02-01 16:12:18 +0300

    DZ2. Lessons7

---
+++ Introduction/Lessons7.Recursion/DZ2/DZ2.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n. */
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение m: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = Ackerman(m, n);
+        Console.WriteLine($"Ответ: {result}");
+
+    }
+    static int Ackerman(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Ackerman(m - 1, 1);
+        }
+        else
+        {
+            return Ackerman(m - 1, Ackerman(m, n - 1));
+        }
+    }
+
+
+}
