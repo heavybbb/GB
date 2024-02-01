48bb85846c6b1d6f2f85935fa3a349108d266d03
Dmitriy K <cd.78@bk.ru>
2024-02-02 02:13:29 +0300

    Lessons 7. DZ3.cs

---
+++ Introduction/Lessons7.Recursion/DZ3/DZ3.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 3: Задайте произвольный массив. 
+Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы. */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 11, 21, 31, 42, 55 };
+        PrintArrayReverse(array, array.Length - 1);
+
+        Console.ReadLine(); 
+    }
+
+    static void PrintArrayReverse(int[] array, int index)
+    {
+        if (index < 0)
+            return;
+
+        Console.Write(array[index] + " ");
+        PrintArrayReverse(array, index - 1);
+    }
+}
