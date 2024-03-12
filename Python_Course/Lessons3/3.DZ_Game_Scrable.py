d8aff68c585dc2160b47d37d141caf509897a4ad
Dmitriy K <cd.78@bk.ru>
2024-03-12 23:49:05 +0300

    Решина задача Скрабл

--- Python_Course/Lessons3/3.DZ_Game_Scrable.py
+++ Python_Course/Lessons3/3.DZ_Game_Scrable.py
@@ -28,9 +28,51 @@
 ###################################################################
 
 
-k = 'ноутбук'
-list1={"AEIOULNSTRАВЕИНОРСТ":1, "DGДКЛМПУ":2}
-list2={}
-print(list1)
-for i in list1:
-    
+# list1 = {1: 'AEIOULNSTRАВЕИНОРСТ', 2: 'DGДКЛМПУ', 3: 'BCMPБГЁЬЯ',
+#           4: 'FHVWYЙЫ', 5: 'KЖЗХЦЧ', 8: 'JZШЭЮ', 10: 'QZФЩЪ'}
+# k = input("Введите слово: ").upper() 
+# count = 0
+# for i in k:
+#     if i in 'QWERTYUIOPASDFGHJKLZXCVBNMАВЕИНОРСТДКЛМПУБГЁЬЯЙЫЖЗХЦЧШЭЮФЩЪ':
+#         for l in list1:
+#             if i in list1[l]:
+#                 count = count + l
+#                 print(l)
+   
+# print(f'Стоимость слова: {count}')
+
+
+
+################ Вторая версия программы
+
+k = 'lizard'
+k=k.upper()
+
+list1 = {1: 'A, E, I, O, U, L, N, S, T, R,А, В, Е, И, Н, О, Р, С, Т', 2: 'DGДКЛМПУ', 3: 'BCMPБГЁЬЯ',
+          4: 'FHVWYЙЫ', 5: 'KЖЗХЦЧ', 8: 'JXШЭЮ', 10: 'QZФЩЪ'}
+count = 0
+for i in k:
+    if i in 'QWERTYUIOPASDFGHJKLZXCVBNMАВЕИНОРСТДКЛМПУБГЁЬЯЙЫЖЗХЦЧШЭЮФЩЪ':
+        for l in list1:
+            if i in list1[l]:
+                count = count + l
+                  
+print(count)
+
+
+
+############ Решение учителя
+points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+word = k.upper()  # переводим все буквы в верхний регистр
+count = 0
+for i in word:
+    if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+        for j in points_en:
+            if i in points_en[j]:
+                count = count + j
+    else:
+        for j in points_ru:
+            if i in points_ru[j]:
+                count = count + j
+print(count)
