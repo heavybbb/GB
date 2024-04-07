5c6e7050d7ee2c7ac3c134d0e4a886525e9e044b
Dmitriy K <cd.78@bk.ru>
2024-04-07 15:30:51 +0300

    окончательный вариант программы

--- Introduction/Control_work/ControlWork.cs
+++ Introduction/Control_work/ControlWork.cs
@@ -1,40 +1,50 @@
-﻿/*
-Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
-длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
-либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
-лучше обойтись исключительно массивами.
-*/
+﻿// /*
+// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+// */
+    
 using System;
-
 class Program
 {
     static void Main()
     {
-
-
-        string inputString = " ";
         string[] inputStringArray = new string[100];
-        for (int i = 0; inputStringArray.Length >= i && inputString != ""; i++)
+        int count = 0;
 
+        Console.WriteLine("Введите строки. Для завершения ввода нажмите Enter без ввода текста.");
+
+        // Ввод строк до тех пор, пока пользователь не нажмет Enter без ввода текста
+        string inputString;
+        do
         {
-            Console.WriteLine("Введите строку, по завершении ввода нажмите Enter ничего не вводя");
             inputString = Console.ReadLine();
-            inputStringArray[i] = inputString;
+            if (!string.IsNullOrEmpty(inputString))
+            {
+                inputStringArray[count] = inputString;
+                count++;
+            }
+        } 
+        while (!string.IsNullOrEmpty(inputString) && count < 100);
+
+        // Создание нового массива из строк, длина которых меньше или равна 3 символам
+        string[] newArray = new string[count]; // Создаем массив нужного размера
+        int newArrayIndex = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (inputStringArray[i].Length <= 3)
+            {
+                newArray[newArrayIndex] = inputStringArray[i];
+                newArrayIndex++;
+            }
         }
 
-            string[] newArray = new string[100];
-            int count = 0;
-            foreach (string item in inputStringArray)
-            {
-                if (item.Length <= 3)
-                {
-                   Console.WriteLine(item);
-                                    
-                }
-
-            }
+        // Вывод нового массива
+        Console.WriteLine("Строки с длиной <= 3 символов:");
+        for (int i = 0; i < newArrayIndex; i++)
+        {
+            Console.WriteLine(newArray[i]);
+        }
     }
 }
-
-
-    
